fae456fddc719c7a92c7d78f24593dad3cfd461c
Pavlotjg <pavlo.honsevych@gmail.com>
2020-01-09 23:35:31 +0200

    code refactoring

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,127 +1,123 @@
 import React from 'react';
 import '../../App.scss';
-import Country from "./Country";
+
 
 const Home = () => {
 
-    return (
-        <div className='main-Grid-Home'>
-            <div >
-              <a href="https://images.unsplash.com/photo-1549334707-3aa2dacd0c57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" target='_blank'>
-                <img src="https://images-cdn.9gag.com/photo/a3MbmQe_700b.jpg" alt=""/>
-                </a>
-
-            </div>
-          {/*<Country />*/}
-          <div className='star-wars'>
-            <div className='crawl'>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-              <p>
-                War! The Republic is crumbling under attacks by
-                the ruthless Sith Lord, Count Dooku. There are heroes
-                on both sides. Evil is everywhere. In a stunning move,
-                the fiendish droid leader, General Grievous, has swept
-                into the Republic capital and kidnapped Chancellor Palpatine,
-                leader of the Galactic Senate. As the Separatist Droid Army
-                attempts to flee the besieged capital with their valuable
-                hostage, two Jedi Knights lead a desperate mission to rescue
-                the captive Chancellor...
-              </p>
-            </div>
-          </div>
-
+  return (
+    <div className='main-Grid-Home'>
+      <div>
+        <a
+          href="https://images.unsplash.com/photo-1549334707-3aa2dacd0c57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+          target='_blank'>
+          <img src="https://images-cdn.9gag.com/photo/a3MbmQe_700b.jpg" alt=""/>
+        </a>
+      </div>
+      <div className='star-wars'>
+        <div className='crawl'>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
+          <p>
+            War! The Republic is crumbling under attacks by
+            the ruthless Sith Lord, Count Dooku. There are heroes
+            on both sides. Evil is everywhere. In a stunning move,
+            the fiendish droid leader, General Grievous, has swept
+            into the Republic capital and kidnapped Chancellor Palpatine,
+            leader of the Galactic Senate. As the Separatist Droid Army
+            attempts to flee the besieged capital with their valuable
+            hostage, two Jedi Knights lead a desperate mission to rescue
+            the captive Chancellor...
+          </p>
         </div>
-
-
-
-    )
+      </div>
+    </div>
+  )
 };
 
 export default Home;
